d272770407e2f483798c1fa691dbde1f8c6842df
wisdom08 <wisdom08@kakao.com>
2022-08-10 12:09:57 +0900

    feat: #29 게시글 수정 기능 추가
    추가적으로 불필요한 주석을 제거했고, 정적 팩토리 메서드 팩토리를 공부해보고 게시글 엔티티를 개선했습니다.

--- src/main/java/com/sparta/springweb/model/Post.java
+++ src/main/java/com/sparta/springweb/model/Post.java
@@ -1,24 +1,18 @@
 package com.sparta.springweb.model;
 
-import com.sparta.springweb.dto.PostRequestDto;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter // get 함수를 일괄적으로 만들어줍니다.
-@NoArgsConstructor // 기본 생성자를 만들어줍니다.
-@Entity // DB 테이블 역할을 합니다.
+@Getter
+@Entity
 public class Post extends Timestamped {
 
-    // ID가 자동으로 생성 및 증가합니다.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
-    // 반드시 값을 가지도록 합니다.
     @Column(nullable = false)
     private String title;
 
@@ -33,32 +27,22 @@ public class Post extends Timestamped {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private final List<Comment> comments = new ArrayList<>();
 
-    public Post(String title, String username, String content) {
+    protected Post() {}
+
+    private Post(String username, String title, String content, String filePath) {
+        this.name = username;
         this.title = title;
-        this.name = username;
         this.content = content;
+        this.filePath = filePath;
     }
 
-    public Post(PostRequestDto requestDto) {
-        this.title = requestDto.getTitle();
-        this.content = requestDto.getContents();
+    public static Post createPost(String username, String title, String content, String filePath) {
+        return new Post(username, title, content, filePath);
     }
 
-    public Post(PostRequestDto requestDto, String username) {
-        this.title = requestDto.getTitle();
-        this.name = username;
-        this.content = requestDto.getContents();
-    }
-
-    public void update(PostRequestDto requestDto) {
-        this.title = requestDto.getTitle();
-        this.content = requestDto.getContents();
-    }
-
-    public Post(PostRequestDto requestDto, String username, String filePath) {
-        this.title = requestDto.getTitle();
-        this.content = requestDto.getContents();
-        this.name = username;
+    public void updatePost(String title, String content, String filePath) {
+        this.title = title;
+        this.content = content;
         this.filePath = filePath;
     }
 }
